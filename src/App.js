2132d2b92af6996c3d17dd2753dce2f03b8b2a3b
Jisoon Kang <wltnskjs@naver.com>
2021-12-09 13:35:35 +0900

    ✅ Study App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello, Voxotron!!!
+      <form>
+        <div class="form-group placeholdered required admin_login"><div class="col-sm-12"><input autocomplete="off" autofocus="autofocus" class="placeholdered required placeholdered" id="admin_login" name="admin[login]" required="required" type="text" /><label alt='Login' placeholder='Login'></label></div></div>
+        <div class="form-group placeholdered required admin_password"><div class="col-sm-12"><input autocomplete="off" class="placeholdered required placeholdered" id="admin_password" name="admin[password]" required="required" type="password" /><label alt='Password' placeholder='Password'></label></div></div>
+        <div class='form-actions'>
+          <button class='btn btn-block btn-primary' type='submit'>Sign in</button>
+        </div>
+      </form>
+      {/* <form accept-charset="UTF-8" action="/admins/sign_in" class="simple_form form-horizontal new_admin" id="new_admin" method="post" novalidate="novalidate">
+        <div style="display:none"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="M3iWGazAD4Yw+PYQBCL7k/ReGn5dZin6X1aZkOqf6jw=" /></div><div class='form-inputs'>
+        <div class="form-group placeholdered required admin_login"><div class="col-sm-12"><input autocomplete="off" autofocus="autofocus" class="placeholdered required placeholdered" id="admin_login" name="admin[login]" required="required" type="text" /><label alt='Login' placeholder='Login'></label></div></div>
+        <div class="form-group placeholdered required admin_password"><div class="col-sm-12"><input autocomplete="off" class="placeholdered required placeholdered" id="admin_password" name="admin[password]" required="required" type="password" /><label alt='Password' placeholder='Password'></label></div></div>
+        </div>
+        <div class='form-actions'>
+        <button class='btn btn-block btn-primary' type='submit'>Sign in</button>
+        </div>
+      </form> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// ./App.js
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* 요기 App이 컴포넌트! */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
